dfc5bae1a8f1604900a5d9bd0e1a65b98ac78b32
morima123 <morimamama123@gmail.com>
2020-09-15 20:14:50 +0900

    Customised(2) and added comments

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
-//const mode = document.getElementsByClassName("jsMode");
 const penBtn = document.getElementById("penBtn");
 const fillBtn = document.getElementById("fillBtn");
 const modeBtn = document.getElementsByClassName("modeBtn");
 const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
+
+// define the size of the canvas here
 const CANVAS_HEIGHT = 500;
 const CANVAS_WIDTH = 800;
+
+// Set the default values here
 canvas.height = CANVAS_HEIGHT;
 canvas.width = CANVAS_WIDTH;
-
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+// For switching painting & filling
 let painting = false;
 let filling = false;
 
-//FOR BEGIN AND END PAINTING
+// Setting the start & end of painting
 function stopPainting() {
   painting = false;
 }
@@ -32,67 +35,65 @@ function startPainting() {
   painting = true;
 }
 
-//FOR PAINTING
+// For mouse move event
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    // Trace the mouse move while mouseup
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
+    // Trace the mouse move while mousedown
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
-//FOR CHOOSING COLOURS
+// For choosing colors
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
   ctx.fillStyle = color;
+  //Passing "disabled" to the target color
+  if (!event.target.disabled) {
+    Array.from(colors).forEach((color) => (color.disabled = false));
+    event.target.disabled = true;
+  }
 }
 
-//FOR THE RANGE INPUT
+// For range input
 function handleRangeChange(event) {
   const size = event.target.value;
   ctx.lineWidth = size;
 }
 
-//PAINT OR FILL MODES
-function inputMode() {
-  if (pen.disabled) {
-    pen.style.backgroundColor("#111");
-  }
-}
-
-//Modify here
+// Paint mode event
 function handleModeClick() {
   if (filling) {
     filling = false;
     penBtn.disabled = true;
     fillBtn.disabled = false;
-    console.log("painting");
   } else {
     filling = true;
     penBtn.disabled = false;
     fillBtn.disabled = true;
-    console.log("filling");
   }
 }
 
-//FOR FILLING ACTION
+// For filling event
 function handleCanvasClick() {
   if (filling) {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 }
 
-//FOR PREVENTING THE RIGHT CLICK
+// Preventing the right click
 function handleCM(event) {
   event.preventDefault();
 }
 
-//FOR SAVING THE IMAGE
+// For saving painting as an image
 function handleSaveClick() {
   const image = canvas.toDataURL();
   const link = document.createElement("a");
@@ -101,7 +102,7 @@ function handleSaveClick() {
   link.click();
 }
 
-//DEFINING THE FUNCTIONS
+// Defining & handling mouse move event
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
